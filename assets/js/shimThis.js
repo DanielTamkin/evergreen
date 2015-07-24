c6cba249b91fa5f551a6b52ed25ac6043ed090e4
DanielTamkin <dudearoo@gmail.com>
2015-07-24 13:24:36 -0500

    Adding shimThis and testing

--- assets/js/shimThis.js
+++ assets/js/shimThis.js
@@ -1,5 +1,5 @@
 /*shimThis.js - by Danieltamkin*/
-/*shim an element to its top element*/
+/*shim an element ontop of its element*/
 (function( $ ) {
 	$.fn.shimThis = function(data) {
     if(data == undefined){
@@ -8,6 +8,18 @@
 		else{
 			$data = data;
 		}
-
+		$this = $(this);
+		function calculate(){
+			var elementHeight 		= $this.height(),
+					topelementHeight 	= $data.height();
+			console.log("elementHeight="+elementHeight);
+			console.log("topelementHeight="+topelementHeight);
+		}
+		return this.each(function() {
+			calculate();
+			$(window).resize(function() {
+				calculate();
+			});
+		});
   };
 }( jQuery ));
