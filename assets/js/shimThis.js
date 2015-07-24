1537a32a57f8bfff4e815532620688ea01781814
DanielTamkin <dudearoo@gmail.com>
2015-07-24 15:50:24 -0500

    Finished shimThis.js, integrating now

--- assets/js/shimThis.js
+++ assets/js/shimThis.js
@@ -1,19 +1,47 @@
 /*shimThis.js - by Danieltamkin*/
 /*shim an element ontop of its element*/
 (function( $ ) {
-	$.fn.shimThis = function(data) {
-    if(data == undefined){
-			$data = "nothing";
-		}
-		else{
-			$data = data;
-		}
+	$.fn.shimThis = function($data) {
 		$this = $(this);
+		data($data);
+		function data(data){
+			if(data == undefined){
+				$data = dataTemplate();
+			}
+			else{
+				if(data.ontop == undefined && data.offset == undefined){
+					$data = dataTemplate();
+				}
+				else{
+					$data = data;
+				}
+			}
+		}
+		function dataTemplate(){
+			$data = {
+				ontop: 'null',
+				offset: '0'
+			}
+			return $data;
+		}
 		function calculate(){
-			var elementHeight 		= $this.height(),
-					topelementHeight 	= $data.height();
-			console.log("elementHeight="+elementHeight);
-			console.log("topelementHeight="+topelementHeight);
+			if($data.ontop != "null"){
+				var elementHeight 		= $this.height(),
+						ontopHeight 			= $($data.ontop).height(),
+						elementShim 			= 0;
+				if($data.offset == "0"){
+					$data.offset = (elementHeight/2)/1.5;
+					$data.offset = $data.offset+"px";
+				}
+				console.log("elementHeight="+elementHeight);
+				console.log("ontopHeight="+ontopHeight);
+				$ontop = $($data.ontop);
+				$this.css("z-index","999");
+				$this.css("margin-top","-"+$data.offset);
+			}
+			else{
+				console.log("shimThis Error: no ontop element to calculate.");
+			}
 		}
 		return this.each(function() {
 			calculate();
