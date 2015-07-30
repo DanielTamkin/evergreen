7a2edb8d254d93d2f3166aa3282522c05bb846d7
DanielTamkin <dudearoo@gmail.com>
2015-07-30 10:00:32 -0500

    Added basic responsivness, updated shimThis version.

--- assets/js/shimThis.js
+++ assets/js/shimThis.js
@@ -31,7 +31,6 @@
 						elementShim 			= 0;
 				if($data.offset == "0"){
 					$data.offset = "25%";
-					$data.offset = $data.offset+"px";
 				}
 				console.log("elementHeight="+elementHeight);
 				console.log("ontopHeight="+ontopHeight);
